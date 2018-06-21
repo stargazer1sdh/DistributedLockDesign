d1010fffd2a4fea00a81cac83fa24a718d7fc08f
sdh <sdh@DESKTOP-A1PEKI7>
2018-06-21 15:15:40 +0800

    del

--- client2.bat
+++ client2.bat
@@ -4,6 +4,6 @@ e:
 cd E:\eclipse-workspace\DistributedLockDesign
 
 
-java -cp gson-2.6.2.jar;hamcrest-core-1.3.jar;junit-4.12.jar;.\bin com.sjtu.sdh.Client 127.0.0.2 4445
+java -cp gson-2.6.2.jar;hamcrest-core-1.3.jar;junit-4.12.jar;.\bin com.sjtu.sdh.Client 192.168.201.128 4447
 
 pause
